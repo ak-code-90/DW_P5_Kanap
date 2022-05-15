3d1c80c98e881bf60c0518fb483477d602284534
Kevin <adda-kevin@hotmail.fr>
2022-05-15 05:21:08 +0200

    code base restructuration

--- front/javascript/product.js
+++ front/javascript/product.js
@@ -1,1 +1,3 @@
  // Etape 4 - Faire le lien entre un produit de la page d'accuiel et la page produit.   
+
+ //On va de voir utiliser urlSearchParams pour obtenir l'ID du produit sur lequel l'utilisateur à cliqué.
