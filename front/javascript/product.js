af456bca47a4f126959ca98d17b30eb026c78033
Kevin <adda-kevin@hotmail.fr>
2022-05-16 03:21:58 +0200

    recovered product ID then recovered product array

--- front/javascript/product.js
+++ front/javascript/product.js
@@ -1,3 +1,50 @@
- // Etape 4 - Faire le lien entre un produit de la page d'accuiel et la page produit.   
+// Récupération de l’id du produit à afficher
 
- //On va de voir utiliser urlSearchParams pour obtenir l'ID du produit sur lequel l'utilisateur à cliqué.
+//On crée une fonction qui va utiliser urlSearchParams pour obtenir l'ID du produit sur lequel l'utilisateur à cliqué.
+function getIdFromUrl() {
+
+    const urlString = window.location.href;  //on recupère ici l'url de la page actuelle
+    const searchParams = new URLSearchParams(urlString);
+
+    // on itère sur les paramètres de recherche et on retourne la valeur de l'id en index 1.
+    for (let id of searchParams) {
+        let idFromURL = id;
+        return idFromURL[1];
+    }
+
+}
+
+let chosenProductId = getIdFromUrl() 
+
+// ici j'utilise un console.log avec timeout pour verifier que ma variable globale crée plus haut contient bien la réponse voulu, 
+// si j'utilise directement un console.log j'ai des chances de ne rien pouvoir afficher
+setTimeout(() => {
+    console.log(chosenProductId);         
+}, 1500);
+
+
+//Récupération du tableau du produit
+// difficultés rencontrées : portée des variables, tentatives de console.log avant les réponses de l'API
+let productArray;
+
+fetch('http://localhost:3000/api/products')
+    .then(function (reponse) { return reponse.json(); })
+    .then(function (array) {
+        for (let element of array) {
+            if (element._id === chosenProductId) {
+                productArray = element;
+            }
+
+
+
+        }
+    })
+
+setTimeout(() => {
+    console.log(productArray);         
+}, 2000);
+
+
+
+
+
--- front/javascript/script.js
+++ front/javascript/script.js
@@ -1,4 +1,4 @@
- // Etape 4 - Inserer les produits dans la page d'accueil 
+ // Etape 3 - Inserer les produits dans la page d'accueil 
 
 // Je récupère les produits en utilisant la methode FETCH, J'utilise .THEN pour attendre et recupérer une reponse en JSON, et je vais ensuite afficher la réponse dans la console pour m'assurer que ça fonctionne.
 fetch('http://localhost:3000/api/products')
@@ -12,16 +12,16 @@ fetch('http://localhost:3000/api/products')
         let displayProducts = '<ul style=\'display:flex; font-size:20px; margin:50px 0 0 0; padding:0px; display:flex;\' >';
 
         //Je crée ensuite une boucle pour ajouter chaque produits du tableau dans ma liste, tout en ajustant le style.
-        //Je fais en sorte que mes  produits soit dans des liens afin de rajouter l'identifiant du produit directement à la fin de l'url en faisant :  ?(chaine_de_caracète)
+        // Etape 4 - Je fais en sorte que mes produits soit dans une balise <a> et je rajoute l'identifiant du produit directement à la fin de l'url en faisant :  ?(chaine_de_caracète)
         for (let products of array) {
 
             displayProducts += `
-                                    <style> #items {list-style-type: none;} ul {display: flex; gap: 10px; list-style-type: none; margin:0; padding:0;} </style>
+                                    <style> #items {list-style-type: none;} ul {display: flex; gap: 10px; list-style-type: none; padding:0;} </style>
 
-                                    <a style=\"display:block; width:100%; \" href="product.html?${products._id}"> 
+                                    <a style=\"display:block; width:100%; \" href="product.html?_id=${products._id}"> 
                                     
                                     <li style=\' display:flex ; gap:20px; margin:30px 30px; \'>
-                                       <img width=300 alt='${products.altTxt}' height= 300  src='${products.imageUrl}'</img>
+                                       <img lazy=loading width=300 alt='${products.altTxt}' height= 300  src='${products.imageUrl}'</img>
                                        <div>
                                           <h3 style=\" font-size:25px; margin:0px; \"> ${products.name} </h3>
                                           <p style=\"font-size:20px\"> ${products.description} </p> 
@@ -51,3 +51,4 @@ fetch('http://localhost:3000/api/products')
 
 
 
+  
